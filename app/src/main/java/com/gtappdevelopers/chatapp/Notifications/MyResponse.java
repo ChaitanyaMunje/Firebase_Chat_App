3fe40d86fe30b105407477d3cde566ca09de9942
HP <chaitanyamunje@gmail.com>
2020-04-30 21:46:07 +0530

    Initial commit

---
+++ app/src/main/java/com/gtappdevelopers/chatapp/Notifications/MyResponse.java
@@ -0,0 +1,5 @@
+package com.gtappdevelopers.chatapp.Notifications;
+
+public class MyResponse {
+    public int success;
+}
---
+++ app/src/main/java/com/gtappdevelopers/chatapp/model/Chatlist.java
@@ -0,0 +1,22 @@
+package com.gtappdevelopers.chatapp.model;
+
+public class Chatlist {
+
+    public Chatlist(){}
+
+    public Chatlist(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String id;
+
+
+}
